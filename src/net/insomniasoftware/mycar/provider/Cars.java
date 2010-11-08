dcad2c8410b2e5c757e46080a7744b46bf66492c
Jisakiel <jisakiel@yahoo.es>
2010-11-08 19:37:44 +0100

    typos

--- src/net/insomniasoftware/mycar/provider/Cars.java
+++ src/net/insomniasoftware/mycar/provider/Cars.java
@@ -4,7 +4,7 @@ import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
- * The Cars provider contains information about car profiles and its know positions.
+ * The Cars provider contains information about car profiles and its known locations.
  */
 public class Cars {
 
@@ -38,25 +38,25 @@ public class Cars {
     	public static final String NAME = "name";
     	
     	/**
-    	 * The latitude of the last know position.
+    	 * The latitude of the last known location.
     	 * <P>Type: INTEGER (integerE6)</P>
     	 */
     	public static final String LATITUDE = "latitude";
     	
     	/**
-    	 * The longitude of the last know position.
+    	 * The longitude of the last known location.
     	 * <P>Type: INTEGER (integerE6)</P>
     	 */
     	public static final String LONGITUDE = "longitude";
 
     	/**
-    	 * The accuracy of the last know position.
+    	 * The accuracy of the last known location.
     	 * <P>Type: REAL (float)</P>
     	 */
     	public static final String ACCURACY = "accuracy";
     	
     	/**
-    	 * The date in milliseconds of the last know position.
+    	 * The date in milliseconds of the last known location.
     	 * <P>Type: INTEGET (long)</P>
     	 */
     	public static final String DATE = "date";
@@ -64,12 +64,12 @@ public class Cars {
     	/**
     	 * The identifier of the resource (color/image...) of the profile.
     	 * <P>Type: INTEGET (int)</P>
-    	 * TODO: a RESOURCE_TYPE column my be needed
+    	 * TODO: a RESOURCE_TYPE column may be needed
     	 */
     	public static final String RESOURCE = "resource";
 
     	/**
-    	 * The MAC address of the car bluetooth
+    	 * The MAC address of the car's bluetooth
     	 * <P>Type: TEXT</P>
     	 */
     	public static final String MAC_ADDRESS = "mac_address";
